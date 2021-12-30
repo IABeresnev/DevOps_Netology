05ac5137cc42f4380ff51acb3a317b7cbb503ec6
Beresnev Ilya <beresnevia.it@gmail.com>
2021-12-30 13:40:31 +0400

    Testing2 python script

--- 16_Lesson_python/sourcecheck.py
+++ 16_Lesson_python/sourcecheck.py
@@ -3,11 +3,12 @@
 import os
 import re
 
+pathToRepo="~/PycharmProjects/DevOps_Netology/"
 regex = r"(?:modified:\s*.*)"
-bash_command = ["cd ~/PycharmProjects/DevOps_Netology/", "git status"]
+bash_command = ["cd "+pathToRepo, "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 if result_os.find('modified') != -1:
     matches = re.finditer(regex, result_os, re.MULTILINE)
     for match in enumerate(matches):
         rez = str(list(match)[1])
-        print(rez[rez.find(":")+4:-2])
+        print(pathToRepo+rez[rez.find(":")+4:-2])
