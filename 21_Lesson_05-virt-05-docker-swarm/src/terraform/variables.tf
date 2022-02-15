6c964de2a2137ba3f13252cdd2bb4cf5ca0fdf66
Beresnev Ilya <beresnevia.it@gmail.com>
2022-02-16 01:03:17 +0400

    Added new DZ 5.5

---
+++ 21_Lesson_05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1g5ul7a7b6nbdp87hh3"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gd762fp1cah5nd98g6"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd83vs5ng3qslk2pa99t"
+}
